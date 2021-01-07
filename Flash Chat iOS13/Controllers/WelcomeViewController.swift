100a61f6455b9d2ec0a4c7fc198a79b975563e2d
jamestang12 <homing.tang@ryerson.ca>
2021-01-06 22:18:38 -0500

    Wlecome View Animation Created

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -14,7 +14,17 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        titleLabel.text = ""
+        let titleText = "⚡️FlashChat"
+        var charIndex = 0.0
+        for letter in titleText{
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false){
+                (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
        
     }
     
