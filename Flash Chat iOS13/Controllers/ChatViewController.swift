e166f700d7d6c0af593b127af714e4e31e5b3b49
jamestang12 <homing.tang@ryerson.ca>
2021-01-07 19:04:44 -0500

    UI Update

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -32,7 +32,9 @@ class ChatViewController: UIViewController {
     }
     
     func loadMessage(){
-        db.collection(K.FStore.collectionName).addSnapshotListener { (querySnapshot, error) in
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { (querySnapshot, error) in
             
             self.messages = []
             if let e = error{
@@ -58,7 +60,7 @@ class ChatViewController: UIViewController {
     
     @IBAction func sendPressed(_ sender: UIButton) {
         if let messageBody = messageTextfield.text,let messageSender = Auth.auth().currentUser?.email{
-            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender, K.FStore.bodyField: messageBody]) { (error) in
+            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender, K.FStore.bodyField: messageBody, K.FStore.dateField: Date().timeIntervalSince1970]) { (error) in
                 if let e = error{
                     print("There was an issue saving data to firestore, \(e)")
                 }else{
